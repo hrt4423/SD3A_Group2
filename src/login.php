23fb15a5bb5825c89ed5de01f41eb5033f3673c6
muramasa08 <nomukinn0815@gmail.com>
2023-05-12 10:18:20 +0900

    add h2

--- src/login.php
+++ src/login.php
@@ -8,9 +8,18 @@
     <title>login</title>
 </head>
 <body>
-    <?php
-        
-    ?>
+        <form action="" method="post">
+        <div class="err_msg"><?php echo $err_msg['email']; ?></div>
+        <label for=""><span>メールアドレス</span>
+          <input type="email" name="email" id=""><br>
+        </label>
+        <div class="err_msg"><?php echo $err_msg['password']; ?></div>
+        <label for=""><span>パスワード</span>
+          <input type="text" name="password" id=""><br>
+        </label>
+        <input type="submit" value="ログイン">
+        </form>
+        <a href="新規登録画面">新規登録<a>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
