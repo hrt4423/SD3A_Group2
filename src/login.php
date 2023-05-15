187f8faebc97cc05e1bccc13568a0b3a4aeda940
muramasa08 <nomukinn0815@gmail.com>
2023-05-15 10:08:51 +0900

    add h2

--- src/login.php
+++ src/login.php
@@ -33,6 +33,7 @@ $error_message = "※ID、もしくはパスワードが間違っています。
       <p>パスワード：<input type="password" name="password"></p>
       <input type="submit" name="login" value="ログイン">
     </form>
+    <a href="sinnki">新規登録<a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
