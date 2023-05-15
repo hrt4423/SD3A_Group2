f3485aa0105a819aab2f7a2c1832b7629f79427b
muramasa08 <nomukinn0815@gmail.com>
2023-05-15 10:25:39 +0900

    add h2

--- src/login.php
+++ src/login.php
@@ -37,4 +37,9 @@ $error_message = "※ID、もしくはパスワードが間違っています。
     <a href="sinnki">新規登録<a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+<style>
+  body{
+    color:CC66FF;
+  }
+</style>
 </html>
