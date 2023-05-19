677db09f736bc390d067451fc836855297cc0dc4
muramasa08 <nomukinn0815@gmail.com>
2023-05-19 09:40:25 +0900

    add h2

---
+++ src/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+
+$error_message = "";
+
+if(isset($_POST["login"])) {
+
+	if($_POST["user_name"] == "webtan" && $_POST["password"] == "webtan_pass") {
+		$_SESSION["user_name"] = $_POST["user_name"];
+		$login_success_url = "login_success.php";
+		header("Location: {$login_success_url}");
+		exit;
+	}
+$error_message = "※ID、もしくはパスワードが間違っています。<br>　もう一度入力して下さい。";
+}
+?>
+<body>
+    <?php
+        if($error_message) {
+            echo $error_message;
+        }
+    ?>
+
+    <form action="index.php" method="POST">
+        <p>ログインID：<input type="text" name="user_name"></p>
+        <p>パスワード：<input type="password" name="password"></p>
+        <input type="submit" name="login" value="ログイン">
+        <a harf="">新規登録</a>
+    </form>
+</body>
+</html>
