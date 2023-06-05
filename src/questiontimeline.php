739703acd088eaeab82b5cf7b9ff340ce6aae5fc
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-06-05 10:24:25 +0900

    修正手伝い

--- src/questiontimeline.php
+++ src/questiontimeline.php
@@ -15,85 +15,88 @@
   <script src="js/questiontimeline.js"></script>
   <style>
     .btn-purple {
-      background-color: #653A91;
-      border-color: #653A91;
-      color: #fff;
-    }
-    .btn-purple:hover {
-      background-color: #4b2661;
-      border-color: #4b2661;
-      color: #fff;
-    }
-    .btn-purple:focus {
-      box-shadow: none;
-      color: #fff;
-    }
+  background-color: #653A91;
+  border-color: #653A91;
+  color: #fff;
+}
+.btn-purple:hover {
+  background-color: #4b2661;
+  border-color: #4b2661;
+  color: #fff;
+}
+.btn-purple:focus {
+  box-shadow: none;
+  color: #fff;
+}
 
-    .header_size {
-      height: 150px;
-      background-color: #b164ff;
-    }
+.header_size {
+  /* height: 150px; */
+  background-color: #b164ff;
+}
 
-    .horizontal {
-      display: flex;
-      text-align: center;
-    }
+.horizontal {
+  display: flex;
+  text-align: center;
+  height: 4.5vw;
+}
 
-    .search {
-      width: 200px;
-      height: 37px;
-      margin-right: 20px;
-    }
+.search {
+  width: 200px;
+  height: 37px;
+  margin-right: 20px;
+}
 
-    .right {
-      margin-left: auto;
-      display: flex;
-      margin-top: 15px;
-    }
+.right {
+  margin-left: auto;
+  display: flex;
+  margin-top: 1.5vw;
+}
 
-    .text {
-      color: white;
-      font-size: 30px;
-      font-weight: bold;
-      flex-grow: 1;
-      margin-top: 35px;
-    }
+.text {
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+  flex-grow: 1;
+}
 
-    .circle {
-      width: 37px;
-      height: 37px;
-      border-radius: 50%;
-      background-color: #653A91;
-      margin-right: 20px;
-    }
+.circle {
+  width: 37px;
+  height: 37px;
+  border-radius: 50%;
+  background-color: #653A91;
+  margin-right: 20px;
+}
 
-    .btn-purple {
-      background-color: #653a91;
-      color: #fff;
-    }
+.btn-purple {
+  background-color: #653a91;
+  color: #fff;
+}
 
-    .btn {
-      margin-right: 20px;
-    }
+.btn {
+  margin-right: 20px;
+}
 
-    .underline {
-      text-decoration: none; /* 下線をなくす */
-      display: inline-block;
-      width: 100%;
-    }
+.underline {
+  text-decoration: none; /* 下線をなくす */
+  display: inline-block;
+  border-bottom: 10px solid #653A91;
+}
 
-    .underline.active {
-      text-decoration: underline; 
-      border-bottom: 10px solid #653A91;
-      text-decoration: none;
-    }
-
-    a:hover {
-      color: white;
-      border-bottom: none;
-      text-decoration: none;
-    }
-
+a:hover {
+  text-decoration: none;
+  color: white;
+  width: 2vw;
+}
+.logo{
+  margin-top: 0.9vw;
+  width: 10vw;
+  height: 2.7vw;
+}
+.test {
+  width: 100px;
+  height: 100px;
+  background-color: #b164ff;
+}
   </style>
 </head>
 <?php
@@ -102,103 +105,105 @@ try{
   require_once './DAO/posts.php';
   $postAll = new DAO_post();
   $search = $postAll->post();//データ取得
-  $count = count($search);
 }catch(Exception $ex){
   echo $ex->getMessage();
 }catch(Error $err){
   echo $err->getMessage();
 }
 ?>
-<body>
-  <div class="header_size">
-    <div class="horizontal">
-        <img class="logo" src="./images/logo.png" height="60" alt="ロゴ">
-      <div class="right">
-  
-        <div class="input-group mb-3 search" >
-          <div class="input-group-prepend">
-            <span class="input-group-text">
-            <i class="fa fa-search"></i>
-            </span>
-          </div>
-          <input type="text" class="form-control" placeholder="検索" aria-label="検索" aria-describedby="basic-addon2">
-        </div>
-  
-        <div class="circle"></div>
-        
-  
-        <div class="dropdown">
-          <button class="btn btn-purple dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            投稿する
-          </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">質問</a>
-              <a class="dropdown-item" href="#">記事</a>
-             </div>
-            </div>
-  
-      </div>
-    </div>
-  
-    <div class="horizontal">
-      <a href="#" class="underline text">質問</a>
-      <a href="#" class="underline text">記事</a>
-      <a href="#" class="underline text">教室</a>
-      <a href="#" class="underline text">ランキング</a>
-    </div>
-  
-  </div>
+<body id="body">
+<div class="header_size">
+      <div class="horizontal">
+          <img class="logo" src="images/logo.png" height="60" alt="ロゴ">
+        <div class="right">
 
-  <fieldset class="frameborder">
+          <div class="input-group mb-3 search" >
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+              <i class="fa fa-search"></i>
+              </span>
+            </div>
+            <input type="text" class="form-control" placeholder="検索" aria-label="検索" aria-describedby="basic-addon2">
+          </div>
+
+          <div class="circle"></div>
+            <div class="dropdown">
+                <button class="btn btn-purple dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  投稿する
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">質問</a>
+                  <a class="dropdown-item" href="#">記事</a>
+                </div>
+            </div>
+        </div>
+      </div>
+
+    </div>
+<!-- ↑ヘッダー -->
+
+  <!-- <fieldset class="frameborder"> -->
     <!--タグ検索ボタン-->
-    <div class="btn-wrap">
-        <a href="" class="btn btn-switch"><span>タグ検索</span></a>
+    <div class="select_area">
+    <div class="sele_area1">
+      <select class="select1">
+          <option value="1">タグ</option>
+          <option value="2">タグ</option>
+        </select>
       </div>
       <!--タグ検索ボタン終了-->
 <!--並び替えセレクトボックス-->
-      <div class="mamListBox">
-        <a href="javascript:void(0)">
+        <!-- <a href="javascript:void(0)">
           <div>↓並び替え</div>
         </a>
         <select size="2" data-role="none">
           <option value="1">最新投稿</option>
           <option value="2">古い投稿</option>
-        </select>
+        </select> -->
+        <div class="sele_area2">
+          <select class="select2">
+            <option value="1">最新投稿</option>
+            <option value="2">古い投稿</option>
+          </select>
       </div>
+  </div>
       <!--並び替えセレクトボックス終了-->
  <!--質問画面遷移ボタン-->
+ <div class="test">aaa</div>
  <div class="question_area">
- <?php foreach ($search as $post): ?>
-  <div class="question1">
-    <button>
-      <p id="user-name">
-        😊👑 @user
-      </p>
-      <p id="posted-date">
-        <?= $post['post_time']; ?>に投稿
-      </p>
-      <p id="title">
-        <?= $post['post_title']; ?>
-      </p>
-      <p id="tag">
-        📌
-      </p>
-      <p id="number-of-responses">
-        回答件数：
-      </p>
-      <p id="good">
-        👍
-      </p>
-      <p id="good-number">
- 
-      </p>
-    </button>   
-  </div>
-  </div>
-<?php endforeach; ?>
+    <?php foreach($search as $post){
+        echo '<div >
+        <button class="question">
+          <p class="user2">
+           @user
+          </p>
+          <p class="day">
+            ' . $post['post_time'] . 'に投稿
+          </p>
+          <p class="title">
+            ' . $post['post_title'] . '
+          </p>
+          <p class="tag">
+            📌
+          </p>
+          <p class="answer">
+            回答件数：
+          </p>
+          <div class="good_area">
+                  <div class="good_img">
+                    <img src="./images/good.png" alt="" class="img3">
+                  </div>
+                </div>
+                <p class="good">134</p>
+        </button>   
+      </div>';
+    }
+    ?>
+ </div>
+
    <!--質問画面遷移ボタン終了-->
-  </form>
-  </fieldset>
+  <!-- </form>
+  </fieldset> -->
   <script>
     $(document).ready(function() {
      // リンクをクリックした時の処理
