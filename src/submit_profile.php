adf76f627c3e400b9ca7f24a32593675eea2f106
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-06-13 10:17:18 +0900

    wip

--- src/submit_profile.php
+++ src/submit_profile.php
@@ -6,7 +6,6 @@
     $id = 1;
     $users->updateProfile($id, $_POST);
     echo 'プロフィールを更新しました。';
-    //header('Location: ./profile_question.php');
     header('Refresh: 5; ./profile_question.php');
 
 ?>
