21a3533bc3f8b96b744967217892704111e8395f
muramasa08 <nomukinn0815@gmail.com>
2023-06-15 10:50:19 +0900

    add

--- src/dao/posts.php
+++ src/dao/posts.php
@@ -20,6 +20,21 @@
       //$count = count($search);
       return $search;
     }
+
+    public function prof_post () {
+      $pdo=$this->dbConnect();
+      $sql = "SELECT posts.*, users.user_name, COUNT(goods.post_id) AS good_count
+      FROM posts 
+      JOIN users ON posts.user_id = users.user_id
+      LEFT JOIN goods ON posts.post_id = goods.post_id
+      WHERE posts.post_category_id=1
+      GROUP BY posts.post_id";
+      $ps = $pdo->prepare($sql);
+      $ps->execute();
+      $search = $ps->fetchAll();
+      //$count = count($search);
+      return $search;
+    }
   }
 
 ?>
