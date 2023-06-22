0fc8efacc49b02ce18ea6e0e582c76acaf4d5899
muramasa08 <nomukinn0815@gmail.com>
2023-06-22 10:50:19 +0900

    add

--- src/dao/articleposts.php
+++ src/dao/articleposts.php
@@ -20,23 +20,6 @@
       //$count = count($search);
       return $search;
     }
-
-    public function prof_post () {
-      $id=$_SESSION['user_id'];
-      $pdo=$this->dbConnect();
-      $sql = "SELECT posts.*, users.user_name, COUNT(goods.post_id) AS good_count
-      FROM posts 
-      JOIN users ON posts.user_id = users.user_id
-      LEFT JOIN goods ON posts.post_id = goods.post_id
-      WHERE posts.post_category_id=2 AND users.user_id=?
-      GROUP BY posts.post_id";
-      $ps = $pdo->prepare($sql);
-      $ps->execute([$id]);
-      $search = $ps->fetchAll();
-      //$count = count($search);
-      return $search;
-
-    }
   }
 
 ?>
