8e4771c57e9d269e181470c97ea81db5de47f3fa
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-07-03 12:43:07 +0900

    記事作成機能：修正

--- src/dao/posts.php
+++ src/dao/posts.php
@@ -1,123 +1,125 @@
 <?php
   class posts {
-      public function insertPosts($title, $detail, $user_id, $post_priority, $post_category_id) {
-          // DB接続情報
-          $dsn = 'mysql:dbname=asoda;host=localhost';
-          $user = 'root';
-          $password = 'root';
-
-          try {
-              $dbh = new PDO($dsn, $user, $password);
-          } catch (PDOException $e) {
-              print('Error:' . $e->getMessage());
-              die();
-          }
-
-          // テスト検索
-          $sql = "INSERT INTO posts (user_id,
-                                    post_category_id,
-                                    post_time,
-                                    post_title,
-                                    post_detail,
-                                    post_priority
-                                    ) 
-                  VALUES (:user_id,
-                          :post_category_id, 
-                          :post_time,
-                          :post_title,
-                          :post_detail,
-                          :post_priority
-                        )";
-
-          // プリペアドステートメントを作成
-          $stmt = $dbh->prepare($sql);
-
-          // 日時をセット
-          $post_time = date("Y-m-d H:i:s");
-
-          // パラメータをバインド
-          $stmt->bindParam(':user_id', $user_id);
-          $stmt->bindParam(':post_category_id', $post_category_id);
-          $stmt->bindParam(':post_time', $post_time);
-          $stmt->bindParam(':post_title', $title);
-          $stmt->bindParam(':post_detail', $detail);
-          $stmt->bindParam(':post_priority', $post_priority);
-
-          // ステートメントを実行
-          $stmt->execute();
-
-          $post_id = (int)$dbh->lastInsertId(); // 最後に挿入されたレコードのIDを整数(int)として取得
-
-          try {
-              echo "データが正常に挿入されました。";
-          } catch (PDOException $e) {
-              echo "エラー: " . $e->getMessage();
-          }
-
-          return $post_id; // $post_idを整数(int)として返す
-        
     private function connect(){
       require_once('connection.php');
-      $connection = new Connection;
-      return $connection->dbConnect();
+      $connection = new connection();
+      $dbh = $connection->dbConnect();
+      return $dbh;
     }
 
-    public function insertPosts($title, $detail, $user_id, $post_priority) {
-        // DB接続情報
-        $dsn = 'mysql:dbname=asoda;host=localhost';
-        $user = 'root';
-        $password = '';
+    public function insertPosts($title, $detail, $user_id, $post_priority, $post_category_id) {
+      // DB接続情報
+      $dsn = 'mysql:dbname=asoda;host=localhost';
+      $user = 'root';
+      $password = 'root';
 
-        try {
-            $dbh = new PDO($dsn, $user, $password);
-        } catch (PDOException $e) {
-            print('Error:' . $e->getMessage());
-            die();
-        }
+      try {
+          $dbh = new PDO($dsn, $user, $password);
+      } catch (PDOException $e) {
+          print('Error:' . $e->getMessage());
+          die();
+      }
 
-        // テスト検索
-        $sql = "INSERT INTO posts (user_id,
-                                  post_category_id,
-                                  post_time,
-                                  post_title,
-                                  post_detail,
-                                  post_priority
-                                  ) 
-                VALUES (:user_id,
-                        :post_category_id, 
-                        :post_time,
-                        :post_title,
-                        :post_detail,
-                        :post_priority
-                      )";
+      // テスト検索
+      $sql = "INSERT INTO posts (user_id,
+                                post_category_id,
+                                post_time,
+                                post_title,
+                                post_detail,
+                                post_priority
+                                ) 
+              VALUES (:user_id,
+                      :post_category_id, 
+                      :post_time,
+                      :post_title,
+                      :post_detail,
+                      :post_priority
+                    )";
 
-        // プリペアドステートメントを作成
-        $stmt = $dbh->prepare($sql);
+      // プリペアドステートメントを作成
+      $stmt = $dbh->prepare($sql);
 
-        // 値をセット
-        $post_category_id = 1;
-        $post_time = date("Y-m-d H:i:s");
+      // 日時をセット
+      $post_time = date("Y-m-d H:i:s");
 
-        // パラメータをバインド
-        $stmt->bindParam(':user_id', $user_id);
-        $stmt->bindParam(':post_category_id', $post_category_id);
-        $stmt->bindParam(':post_time', $post_time);
-        $stmt->bindParam(':post_title', $title);
-        $stmt->bindParam(':post_detail', $detail);
-        $stmt->bindParam(':post_priority', $post_priority);
+      // パラメータをバインド
+      $stmt->bindParam(':user_id', $user_id);
+      $stmt->bindParam(':post_category_id', $post_category_id);
+      $stmt->bindParam(':post_time', $post_time);
+      $stmt->bindParam(':post_title', $title);
+      $stmt->bindParam(':post_detail', $detail);
+      $stmt->bindParam(':post_priority', $post_priority);
 
-        // ステートメントを実行
-        $stmt->execute();
+      // ステートメントを実行
+      $stmt->execute();
 
-        $post_id = (int)$dbh->lastInsertId(); // 最後に挿入されたレコードのIDを整数(int)として取得
+      $post_id = (int)$dbh->lastInsertId(); // 最後に挿入されたレコードのIDを整数(int)として取得
 
-        try {
-            echo "データが正常に挿入されました。";
-        } catch (PDOException $e) {
-            echo "エラー: " . $e->getMessage();
-        }
+      try {
+          echo "データが正常に挿入されました。";
+      } catch (PDOException $e) {
+          echo "エラー: " . $e->getMessage();
+      }
 
-        return $post_id; // $post_idを整数(int)として返す
+      return $post_id; // $post_idを整数(int)として返す
+    }
+
+    public function insertpost($title, $detail, $user_id, $post_priority) {
+      // DB接続情報
+      $dsn = 'mysql:dbname=asoda;host=localhost';
+      $user = 'root';
+      $password = 'root';
+
+      try {
+          $dbh = new PDO($dsn, $user, $password);
+      } catch (PDOException $e) {
+          print('Error:' . $e->getMessage());
+          die();
+      }
+
+      // テスト検索
+      $sql = "INSERT INTO posts (user_id,
+                                post_category_id,
+                                post_time,
+                                post_title,
+                                post_detail,
+                                post_priority
+                                ) 
+              VALUES (:user_id,
+                      :post_category_id, 
+                      :post_time,
+                      :post_title,
+                      :post_detail,
+                      :post_priority
+                    )";
+
+      // プリペアドステートメントを作成
+      $stmt = $dbh->prepare($sql);
+
+      // 値をセット
+      $post_category_id = 1;
+      $post_time = date("Y-m-d H:i:s");
+
+      // パラメータをバインド
+      $stmt->bindParam(':user_id', $user_id);
+      $stmt->bindParam(':post_category_id', $post_category_id);
+      $stmt->bindParam(':post_time', $post_time);
+      $stmt->bindParam(':post_title', $title);
+      $stmt->bindParam(':post_detail', $detail);
+      $stmt->bindParam(':post_priority', $post_priority);
+
+      // ステートメントを実行
+      $stmt->execute();
+
+      $post_id = (int)$dbh->lastInsertId(); // 最後に挿入されたレコードのIDを整数(int)として取得
+
+      try {
+          echo "データが正常に挿入されました。";
+      } catch (PDOException $e) {
+          echo "エラー: " . $e->getMessage();
+      }
+
+      return $post_id; // $post_idを整数(int)として返す
     }
 
     public function fetchAllPostsByCategory(int $category, int $sortType = 0) {
@@ -157,10 +159,9 @@
         return $result;
       }
     }
-  }
+  }      
 
   class DAO_post{
-
     private function dbConnect(){
       $pdo = new PDO('mysql:host=localhost;dbname=asoda;charset=utf8','root','root');
       return $pdo;
