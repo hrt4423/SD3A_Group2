0d296255359d1f04cdb6990b0bd14d3b2cb50793
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-07-06 12:40:41 +0900

    user_point修正３

--- src/goodinsert.php
+++ src/goodinsert.php
@@ -2,6 +2,6 @@
 require_once('dao/good.php');
 $goods=new Good();
 if (isset($_POST['post_id']) && isset($_POST['user_id'],$_POST['user_point'])) {
- $goods->insertgood($_POST['post_id'],$_POST['user_id'],$_POST['user_point']);
+ $goods->insertgood($_POST['user_id'],$_POST['point_id'],$_POST['user_point']);
 }
 ?>
