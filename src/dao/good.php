43929da3903717b1203457794ffe730964c72918
kanapiii <113953054+kanapiii@users.noreply.github.com>
2023-07-07 11:19:11 +0900

    ポイント処理完了

--- src/dao/good.php
+++ src/dao/good.php
@@ -36,7 +36,7 @@
       } 
     }
 
-    public function insertgood($user_id,$post_id){
+    public function insertgood($user_id,$post_id,$user_point_id){
       $pdo = $this->dbConnect();
       $sql = "INSERT INTO goods (user_id,post_id) VALUES (?, ?)";
       $sqlUpdatePoints = "UPDATE users SET user_point = user_point + 20, point_sum = point_sum + 20 WHERE user_id = ?";
@@ -46,7 +46,7 @@
       $ps->bindValue(2, $post_id, PDO::PARAM_INT);
 
       $psUpdatePoints = $pdo->prepare($sqlUpdatePoints);
-      $psUpdatePoints->bindValue(1, $user_id, PDO::PARAM_INT);
+      $psUpdatePoints->bindValue(1, $user_point_id, PDO::PARAM_INT);
       try{
       if ($ps->execute()) {
         
