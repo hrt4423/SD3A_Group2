0bf23db774375b5c16f0b1475f950d9373bc3b0d
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-07-13 12:01:23 +0900

    wip

--- sql/update2.sql
+++ sql/update2.sql
@@ -1,9 +1,9 @@
 START TRANSACTION;
 
-ALTER TABLE thema_colors RENAME COLUMN thema_color_id TO theme_color_id;
+ALTER TABLE thema_colors RENAME COLUMN thema_color_id, TO theme_color_id,
 
-ALTER TABLE theme_colors RENAME COLUMN thema_color_name TO theme_color_name;
+RENAME COLUMN thema_color_name, TO theme_color_name,
 
-ALTER TABLE theme_colors RENAME COLUMN thema_color_code TO theme_color_code;
+RENAME COLUMN thema_color_code, TO theme_color_code;
 
 COMMIT;
