1b464661ceca0f83c90821e16493dd570e923216
muramasa08 <nomukinn0815@gmail.com>
2023-07-18 10:36:46 +0900

    add

--- src/questionCreation.php
+++ src/questionCreation.php
@@ -7,6 +7,13 @@
     $users = new Users;
     $USESR_ID = $_SESSION['user_id'];
     $userIconPath = $users->getUserIconPathById($USESR_ID);
+require_once './dao/theme_colors.php';
+  $themeColors = new ThemeColors;
+  if(isset($_SESSION['user_id'])){
+    $currentThemeColorId =  $users->getThemeColorId($_SESSION['user_id']);
+  }else{
+    $currentThemeColorId = 1;
+  }
 ?>
 <?php
   require_once './dao/connection.php';
@@ -315,9 +322,9 @@
         }
     </style>
   </head>
-  <body>
+  <body style="background-color: <?=$themeColors->getSubColorCode($currentThemeColorId) ?>">
     <!-- body部分とstyle部分とscript部分をコピーして使ってください -->
-    <div class="header_size">
+    <div class="header_size" style="background-color: <?=$themeColors->getThemeColorCode($currentThemeColorId)?> ;">
     <?php
         require_once('./dao/Users.php');
         $users = new Users;
@@ -328,7 +335,7 @@
         }
       ?>
       <div class="horizontal">
-        <img class="logo" src="./images/logo.png" height="60" alt="ロゴ" />
+        <img class="logo" src="./images/<?=$themeColors->getLogoPath($currentThemeColorId)?>" height="60" alt="ロゴ" />
         <div class="right">
           <div class="input-group mb-3 search">
             <div class="input-group-prepend">
