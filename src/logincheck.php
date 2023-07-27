9c4565471487ee6246fb8af6f08ced43c40a6d97
198711987 <116323116+198711987@users.noreply.github.com>
2023-07-27 10:53:12 +0900

    logincheck

--- src/logincheck.php
+++ src/logincheck.php
@@ -1,4 +1,20 @@
-<link href="css/logincheck.css?<?php echo date('YmdHis'); ?>" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="ja"> 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+    logincheck
+  </title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="./css/header.css?<?php echo date('YmdHis'); ?>" rel="stylesheet">
+  <link href="css/logincheck.css?<?php echo date('YmdHis'); ?>" rel="stylesheet">
+</head>
+<body>
 <?php 
 
 session_start(); 
@@ -22,10 +38,8 @@ try {
 }
 //emailがDB内に存在しているか確認
 if (!isset($row['user_mail'])) {
-  echo "<div class=logincheck>";
   echo 'メールアドレスが間違っています。';
   echo "<br><a href='login.php'>戻る</a>";
-  echo "</div>";
   return false;
 }
 //パスワード確認後sessionにメールアドレスを渡す
@@ -33,15 +47,16 @@ if (password_verify($_POST['password'],$row['user_pass'])) {
   session_regenerate_id(true); //session_idを新しく生成し、置き換える
   $_SESSION['user_id'] = $row['user_id'];
   header('Refresh: 3; URL=questiontimeline.php');
-  echo "<div class=logincheck2>";
   echo 'ログインしました。ホーム画面へ移動します';
-  echo "</div>";
 } else {
-  echo "<div class=logincheck3>";
   echo 'メールアドレス又はパスワードが間違っています。';
   echo "<div class=return_button>";
   echo "<br><a href='login.php'>戻る</a>";
   echo "</div>";
-  echo "</div>";
   return false;
 }
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
