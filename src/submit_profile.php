df293abf4367807c823b019fb6ca215aea8d6a17
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2023-07-28 14:58:42 +0900

    Revert "wip"
    This reverts commit e76ad4538a3a1c628bf5610647c76a914b616afc.

--- src/submit_profile.php
+++ src/submit_profile.php
@@ -1,4 +1,4 @@
-<?php session_start() ?>
+<?= session_start() ?>
 <!DOCTYPE html>
 <html lang="ja"> 
 <head>
@@ -8,24 +8,38 @@
   <title>
     submit_profile
   </title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="./css/header.css?<?php echo date('YmdHis'); ?>" rel="stylesheet">
   <link href="css/logout.css?<?php echo date('YmdHis'); ?>" rel="stylesheet">
 </head>
 <body>
-  <?php
-    //プロフィールのアップデート処理
-    require_once('./dao/users.php');
-    $users = new Users;
-    
-    $id = $_SESSION['user_id'];
-    try{
-      $users->updateProfile($id, $_POST);
-    }catch(PDOException $pdoE) {
-      echo 'メールアドレスが重複しています。編集画面へ戻ります。';
-      header('Refresh: 2; ./profile_edit.php');
-      exit;
-    }
-  ?>
-  <p>更新しました。プロフィールへ戻ります。</p>
+  <div class="body">
+<?php
+  //プロフィールのアップデート処理
+  require_once('./dao/users.php');
+  $users = new Users;
+   
+  $id = $_SESSION['user_id'];
+
+  try{
+    $users->updateProfile($id, $_POST);
+  }catch(PDOException $pdoE) {
+    header('Refresh: 2; ./profile_edit.php');
+    echo 'メールアドレスが重複しています。編集画面へ戻ります。';
+    exit;
+  }
   
+  $users->updateProfile($id, $_POST);
+  echo '更新しました。プロフィール画面に戻ります';
+  header('Refresh: 2; ./profile_question.php');
+
+?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</div>
 </body>
 </html>
